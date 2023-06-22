1462c1d122d62ae66f35a3b4b10802e9daa79b43
root <root@Old-HP.localdomain>
2023-06-22 14:00:48 +0700

    Update

--- Bai1.c
+++ Bai1.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-float log2(float n)
-{
-    return log(n) / log(2);
-}
+#include <math.h>
 
 int main()
 {
-    // (X,Y)
-    int m = 0, n = 0;
-    printf("Nhap so hang: ");
-    scanf("%d", &m);
-    printf("Nhap so cot: ");
-    scanf("%d", &n);
-    float *p = malloc(m * n * sizeof(float));
+    FILE *file = fopen("Bai1.txt", "r"); // Open the file in read mode
+    if (file == NULL)
+    {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+
+    int m, n;
+    fscanf(file, "%d %d", &m, &n); // Read the dimensions of the matrix
+
+    float *input_mat = malloc(m * n * sizeof(float)); // Allocate memory for the matrix
+
+    // Read the matrix elements from the file
+    for (int i = 0; i < m * n; i++)
+    {
+        fscanf(file, "%f", &input_mat[i]);
+    }
+
+    fclose(file); // Close the file
+
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            do
+            if (input_mat[i * m + j] < 0 || input_mat[i * m + j] > 1)
             {
-                printf("Nhap P(%d,%d)", i, j);
-                scanf("%f", &p[i * m + j]);
-                getchar();
-            } while (p[i * m + j] < 0 || p[i * m + j] > 1);
+                printf("Nhap lai P(%d,%d)", i, j);
+                return -1;
+            }
         }
     }
 
     // float sum = 0;
     // for (int i = 0; i < m * n; i++)
     // {
-    //     printf("%f", p[i]);
-    //     sum += p[i];
+    //     printf("%f", input_mat[i]);
+    //     sum += input_mat[i];
     //     if (sum > 1)
     //     {
     //         printf("Nhap sai ma tran");
@@ -51,20 +59,58 @@ int main()
     {
         for (int j = 0; j < n; j++)
         {
-            big_x[i] += p[i * m + j];
-            big_y[j] += p[i * m + j];
+            big_x[j] += input_mat[i * m + j];
+            big_y[i] += input_mat[i * m + j];
         }
     }
 
     float h_x = 0, h_y = 0;
+
     for (int i = 0; i < m; i++)
     {
-        h_x += log2(big_x[i]);
+        h_x += -big_x[i] * log2f(big_x[i]);
     }
-    printf("H(X) = %f", h_x);
+    printf("H(X) = %f\n", h_x);
+
     for (int i = 0; i < n; i++)
     {
-        h_y += log2(big_y[i]);
+        h_y += -big_y[i] * log2f(big_y[i]);
     }
-    printf("H(Y) = %f", h_y);
+    printf("H(Y) = %f\n", h_y);
+
+    float *given_y_mat = calloc(m * n, sizeof(float));
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            given_y_mat[i * m + j] = input_mat[i * m + j] / big_y[i];
+        }
+    }
+
+    float *given_x_mat = calloc(m * n, sizeof(float));
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            given_x_mat[i * m + j] = input_mat[i * m + j] / big_x[j];
+        }
+    }
+
+    float h_given_y = 0;
+    for (int i = 0; i < m * n; i++)
+        if (input_mat[i])
+            h_given_y += -input_mat[i] * log2f(given_y_mat[i]);
+    printf("H(X|Y) = %f\n", h_given_y);
+
+    float h_given_x = 0;
+    for (int i = 0; i < m * n; i++)
+        if (input_mat[i])
+            h_given_x += -input_mat[i] * log2f(given_x_mat[i]);
+    printf("H(Y|X) = %f\n", h_given_x);
+
+    float h_x_y = 0;
+    for (int i = 0; i < m * n; i++)
+        if (input_mat[i])
+            h_x_y += -input_mat[i] * log2f(input_mat[i]);
+    printf("H(X,Y) = %f", h_x_y);
 }
