a4d78d53b416e604b932f6c5c14d8340aa608d72
root <root@Old-HP.localdomain>
2023-07-10 16:24:29 +0700

    Add comment

--- Bai2b.cpp
+++ Bai2b.cpp
@@ -6,131 +6,121 @@
 // C++(STL) program for Huffman Coding with STL
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // A Huffman tree node
-struct MinHeapNode {
- 
+struct MinHeapNode
+{
     // One of the input characters
     char data;
- 
+
     // Frequency of the character
     unsigned freq;
- 
+
     // Left and right child
     MinHeapNode *left, *right;
- 
+
     MinHeapNode(char data, unsigned freq)
- 
     {
- 
         left = right = NULL;
         this->data = data;
         this->freq = freq;
     }
 };
- 
-// For comparison of
-// two heap nodes (needed in min heap)
-struct compare {
- 
-    bool operator()(MinHeapNode* l, MinHeapNode* r)
- 
+
+// For comparison of two heap nodes (needed in min heap)
+struct compare
+{
+    bool operator()(MinHeapNode *l, MinHeapNode *r)
     {
         return (l->freq > r->freq);
     }
 };
- 
-// Prints huffman codes from
-// the root of Huffman Tree.
-void printCodes(struct MinHeapNode* root, string str)
+
+// Prints huffman codes from the root of Huffman Tree.
+void printCodes(struct MinHeapNode *root, string str)
 {
- 
     if (!root)
         return;
- 
+
     if (root->data != '$')
         cout << root->data << ": " << str << "\n";
- 
+
     printCodes(root->left, str + "0");
     printCodes(root->right, str + "1");
 }
- 
-// The main function that builds a Huffman Tree and
-// print codes by traversing the built Huffman Tree
+
+// The main function that builds a Huffman Tree and print codes by traversing the built Huffman Tree
 void HuffmanCodes(char data[], int freq[], int size)
 {
     struct MinHeapNode *left, *right, *top;
- 
+
     // Create a min heap & inserts all characters of data[]
-    priority_queue<MinHeapNode*, vector<MinHeapNode*>,compare> minHeap;
- 
+    priority_queue<MinHeapNode *, vector<MinHeapNode *>, compare> minHeap;
+
     for (int i = 0; i < size; ++i)
         minHeap.push(new MinHeapNode(data[i], freq[i]));
- 
+
     // Iterate while size of heap doesn't become 1
-    while (minHeap.size() != 1) {
- 
-        // Extract the two minimum
-        // freq items from min heap
+    while (minHeap.size() != 1)
+    {
+        // Extract the two minimum freq items from min heap
         left = minHeap.top();
         minHeap.pop();
- 
+
         right = minHeap.top();
         minHeap.pop();
- 
-        // Create a new internal node with
-        // frequency equal to the sum of the
-        // two nodes frequencies. Make the
-        // two extracted node as left and right children
-        // of this new node. Add this node
-        // to the min heap '$' is a special value
-        // for internal nodes, not used
-        top = new MinHeapNode('$',
-                              left->freq + right->freq);
- 
+
+        // Create a new internal node with frequency equal to the sum of the two nodes frequencies.
+        // Make the two extracted node as left and right children of this new node.
+        // Add this node to the min heap '$' is a special value for internal nodes, not used
+        top = new MinHeapNode('$', left->freq + right->freq);
+
         top->left = left;
         top->right = right;
- 
+
         minHeap.push(top);
     }
- 
-    // Print Huffman codes using
-    // the Huffman tree built above
+
+    // Print Huffman codes using the Huffman tree built above
     printCodes(minHeap.top(), "");
 }
- 
+
 // Driver Code
-int main(){
+int main()
+{
     char buf[1024];
-    
-    // Input 
+
+    // Input
     printf("Nhap vao Chuoi ki tu\n");
     fgets(buf, sizeof(buf), stdin);
-    unsigned int len=strlen(buf)-1;
-    
-    if(len<1024) 
-    buf[len]=0;
-    // Lowercase so dont care about uppercase or lowercase
-    for(int i = 0; buf[i]; i++){
-    buf[i] = tolower(buf[i]);
-}
+    unsigned int len = strlen(buf) - 1;
 
+    if (len < 1024)
+        buf[len] = 0;
 
-    float init[128]={0};
+    // Convert to lowercase
+    for (int i = 0; buf[i]; i++)
+    {
+        buf[i] = tolower(buf[i]);
+    }
 
-    // int length =0;
-    for (int i = 0; buf[i]; i++){
-        int j = (int) buf[i];
-        init[j]+=1.0;
+    float init[128] = {0};
+
+    for (int i = 0; buf[i]; i++)
+    {
+        int j = (int)buf[i];
+        init[j] += 1.0;
     }
 
     char diffChar[128];
     int freq[128];
     int pos = 0;
-    
-    // build different Char array and freq array
-    for (int i = 0 ;i<128;i++){
-        if (init[i]!=0){
+
+    // Build different Char array and freq array
+    for (int i = 0; i < 128; i++)
+    {
+        if (init[i] != 0)
+        {
             diffChar[pos] = i;
             freq[pos] = init[i];
             pos++;
@@ -139,6 +129,3 @@ int main(){
 
     HuffmanCodes(diffChar, freq, pos);
 }
-
-    
-
--- Bai2c.cpp
+++ Bai2c.cpp
@@ -7,174 +7,207 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-void swap(int *x, int *y){
+
+// Function to swap two integers
+void swap(int *x, int *y)
+{
     int temp = *x;
     *x = *y;
-    *y=temp;
+    *y = temp;
 }
-static map<char,string> resultMap;
-void Shanon_Fano(float *freq,char *diffChar,int size);
+
+// Map to store the result of Shanon-Fano encoding
+static map<char, string> resultMap;
+
+// Function to perform Shanon-Fano encoding
+void Shanon_Fano(float *freq, char *diffChar, int size);
 
 // Driver Code
-int main(){
+int main()
+{
     char buf[1024];
-    
-    // Input 
+
+    // Input
     printf("Nhap vao Chuoi ki tu\n");
     fgets(buf, sizeof(buf), stdin);
-    unsigned int len=strlen(buf)-1;
-    
-    if(len<1024) 
-    buf[len]=0;
-    // Lowercase so dont care about uppercase or lowercase
-    for(int i = 0; buf[i]; i++){
-    buf[i] = tolower(buf[i]);
-}
+    unsigned int len = strlen(buf) - 1;
 
-    float init[128]={0};
-    // int length =0;
-    for (int i = 0; buf[i]; i++){
-        int j = (int) buf[i];
-        init[j]+=1.0/len;
+    if (len < 1024)
+        buf[len] = 0;
+
+    // Convert to lowercase
+    for (int i = 0; buf[i]; i++)
+    {
+        buf[i] = tolower(buf[i]);
+    }
+
+    float init[128] = {0};
+    for (int i = 0; buf[i]; i++)
+    {
+        int j = (int)buf[i];
+        init[j] += 1.0 / len;
     }
 
     float freq[128];
     int pos = 0;
     char diffChar[128];
 
-    // build different Char array and freq array and map 
-    for (int i = 0 ;i<128;i++){
-        if (init[i]!=0){
+    // Build different Char array and freq array
+    for (int i = 0; i < 128; i++)
+    {
+        if (init[i] != 0)
+        {
             diffChar[pos] = i;
-            freq[pos]= init[i];
+            freq[pos] = init[i];
             pos++;
         }
     }
 
-    //  Arrange freq and diffChar
-    bool swapped =false;
+    // Arrange freq and diffChar in descending order
+    bool swapped = false;
     for (int i = 0; i < pos; i++)
     {
-        /* code */
-        swapped=false;
-        for (int j = 0;j<pos-1-i;j++){
-            if (freq[j]<freq[j+1]){
-                swap(freq[j],freq[j+1]);
+        swapped = false;
+        for (int j = 0; j < pos - 1 - i; j++)
+        {
+            if (freq[j] < freq[j + 1])
+            {
+                swap(freq[j], freq[j + 1]);
                 char temp = diffChar[j];
-                diffChar[j]=diffChar[j+1];
-                diffChar[j+1]=temp;
+                diffChar[j] = diffChar[j + 1];
+                diffChar[j + 1] = temp;
                 swapped = true;
             }
         }
-        if (swapped==false){
-        break;
-    }
-    }
-    for (int i=0; i<pos; i++){
-        cout<<diffChar[i]<< endl;
-        cout<<freq[i]<< endl;
-    }
-
-    Shanon_Fano(freq,diffChar,pos);
-
-
-
-    float h=0;
-    float h1=0;
-    for (int i=0; i<pos; i++){
-        h-= freq[i] *log2(freq[i]);
-        h1+= freq[i] * resultMap[diffChar[i]].size();
-    }
-
-    cout<< "Hieu Suat: "<< h/h1<<endl;
-    cout<< "Tinh du thua: "<< 1-h/h1<<endl;
-}
-
-void Shanon_Fano(float *freq,char *diffChar,int size){
-if (size ==1 ){
-    cout<< diffChar[0] << " "<< resultMap[diffChar[0]]<<endl;
-    return;
-    }
-list<float> one;
-list<float> zero;
-list<char> oneChar;
-list<char> zeroChar;
-int zeroLen =0;
-int oneLen =0;
-
-float leftSum = freq[0];
-zero.push_back(freq[0]);
-zeroChar.push_back(diffChar[0]);
-zeroLen++;
-float rightSum = 0;
-
-for (int i = 1; i < size;i++){
-    rightSum += freq[i];
-    one.push_back(freq[i]);
-    oneChar.push_back(diffChar[i]);
-    oneLen++;
-}
-
-float min = fabsf(rightSum-leftSum);
-int pos = 1;
-
-while (1){
-    
-    leftSum += freq[pos];
-    rightSum -= freq[pos];
-
-    if (leftSum>=rightSum ){
-        if (min>= fabsf(leftSum-rightSum)){
-            zeroChar.push_back(diffChar[pos]);
-            zero.push_back(freq[pos]);
-            oneChar.pop_front();
-            one.pop_front();
-            oneLen--;
-            zeroLen++;
+        if (swapped == false)
+        {
+            break;
         }
-        break;
     }
 
-    zero.push_back(freq[pos]);
-    zeroChar.push_back(diffChar[pos]);
-    one.pop_front();
-    oneChar.pop_front();
-    min=fabs(leftSum-rightSum);
-    pos++;
-    oneLen--;
+    // Display the sorted characters and their frequencies
+    for (int i = 0; i < pos; i++)
+    {
+        cout << diffChar[i] << endl;
+        cout << freq[i] << endl;
+    }
+
+    // Perform Shanon-Fano encoding
+    Shanon_Fano(freq, diffChar, pos);
+
+    // Calculate the entropy and average length of the encoded message
+    float h = 0;
+    float h1 = 0;
+    for (int i = 0; i < pos; i++)
+    {
+        h -= freq[i] * log2(freq[i]);
+        h1 += freq[i] * resultMap[diffChar[i]].size();
+    }
+
+    cout << "Hieu Suat: " << h / h1 << endl;
+    cout << "Tinh du thua: " << 1 - h / h1 << endl;
+}
+
+// Function to perform Shanon-Fano encoding
+void Shanon_Fano(float *freq, char *diffChar, int size)
+{
+    // Base case: If only one character is left
+    if (size == 1)
+    {
+        cout << diffChar[0] << " " << resultMap[diffChar[0]] << endl;
+        return;
+    }
+
+    // Divide the characters into two groups
+    list<float> one;
+    list<float> zero;
+    list<char> oneChar;
+    list<char> zeroChar;
+    int zeroLen = 0;
+    int oneLen = 0;
+
+    float leftSum = freq[0];
+    zero.push_back(freq[0]);
+    zeroChar.push_back(diffChar[0]);
     zeroLen++;
-}
+    float rightSum = 0;
 
-float zeroA[128];
-float oneA[128];
-char zeroCharA[128];
-char oneCharA[128];
-
-for ( int i = 0; i<zeroLen;i++){
-    if (resultMap[zeroChar.front()].length()==0){
-        resultMap[zeroChar.front()] = "";
-    }
-    zeroCharA[i]=zeroChar.front();
-    resultMap[zeroChar.front()].append("0");
-    zeroA[i]= zero.front();
-    zero.pop_front();
-    zeroChar.pop_front();
-
-}
-
-for ( int i = 0; i<oneLen ;i++){
-
-     if (resultMap[oneChar.front()].length()==0){
-        resultMap[oneChar.front()] = "";
+    for (int i = 1; i < size; i++)
+    {
+        rightSum += freq[i];
+        one.push_back(freq[i]);
+        oneChar.push_back(diffChar[i]);
+        oneLen++;
     }
 
-    resultMap[oneChar.front()].append("1");
-    oneCharA[i]=oneChar.front();
-    oneA[i]= one.front();
-    one.pop_front();
-    oneChar.pop_front();
-}
+    float min = fabsf(rightSum - leftSum);
+    int pos = 1;
 
-Shanon_Fano(oneA, oneCharA, oneLen);
-Shanon_Fano(zeroA, zeroCharA, zeroLen);
+    while (1)
+    {
+        leftSum += freq[pos];
+        rightSum -= freq[pos];
+
+        // Check if the current division yields a better balance
+        if (leftSum >= rightSum)
+        {
+            if (min >= fabsf(leftSum - rightSum))
+            {
+                zeroChar.push_back(diffChar[pos]);
+                zero.push_back(freq[pos]);
+                oneChar.pop_front();
+                one.pop_front();
+                oneLen--;
+                zeroLen++;
+            }
+            break;
+        }
+
+        zero.push_back(freq[pos]);
+        zeroChar.push_back(diffChar[pos]);
+        one.pop_front();
+        oneChar.pop_front();
+        min = fabs(leftSum - rightSum);
+        pos++;
+        oneLen--;
+        zeroLen++;
+    }
+
+    float zeroA[128];
+    float oneA[128];
+    char zeroCharA[128];
+    char oneCharA[128];
+
+    // Encode the characters in the zero group
+    for (int i = 0; i < zeroLen; i++)
+    {
+        if (resultMap[zeroChar.front()].length() == 0)
+        {
+            resultMap[zeroChar.front()] = "";
+        }
+        zeroCharA[i] = zeroChar.front();
+        resultMap[zeroChar.front()].append("0");
+        zeroA[i] = zero.front();
+        zero.pop_front();
+        zeroChar.pop_front();
+    }
+
+    // Encode the characters in the one group
+    for (int i = 0; i < oneLen; i++)
+    {
+        if (resultMap[oneChar.front()].length() == 0)
+        {
+            resultMap[oneChar.front()] = "";
+        }
+
+        resultMap[oneChar.front()].append("1");
+        oneCharA[i] = oneChar.front();
+        oneA[i] = one.front();
+        one.pop_front();
+        oneChar.pop_front();
+    }
+
+    // Recursively perform Shanon-Fano encoding on each group
+    Shanon_Fano(oneA, oneCharA, oneLen);
+    Shanon_Fano(zeroA, zeroCharA, zeroLen);
 }
